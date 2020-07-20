152a603e304ced8171702391521d55206b3dbf61
Ynda Jas <yndajas@gmail.com>
2020-07-20 16:06:09 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,19 @@
+class Post
+  attr_accessor :title, :author
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title
+    save
+  end
+  
+  def save
+    @@all << self if !@@all.include?(self) #add self to all if not already added
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
