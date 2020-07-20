6b2f8e9cca6653a247c39e9644875cf2f969e75d
Ynda Jas <yndajas@gmail.com>
2020-07-20 16:02:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,13 +13,13 @@ class Artist
   
   def add_song(song)
     song.artist = self
-    Song.all << song
+    song.save
   end
   
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     song.artist = self
-    Song.all << song
+    song.save
   end
   
   def self.song_count
