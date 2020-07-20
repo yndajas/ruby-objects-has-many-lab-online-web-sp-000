a170d7998ece43bad8bb01ab305d103c586cb0d9
Ynda Jas <yndajas@gmail.com>
2020-07-20 15:58:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,8 +23,7 @@ class Artist
   end
   
   def self.song_count
-    binding.pry
-    Song.all.uniq.length # don't understand where the data is coming from for the test for this (and why there are duplicate entries in Song.all)
+    Song.all.length # don't understand where the data is coming from for the test for this (and why there are duplicate entries in Song.all)
   end
     
 end
